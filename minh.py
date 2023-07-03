9bf0eb560fda471b53b9a4bbe9351f18e6dd4347
IMBETTER425 <138457152+IMBETTER425@users.noreply.github.com>
2023-07-03 13:50:05 +0000

    Fixed error

--- minh.py
+++ minh.py
@@ -1,7 +1,7 @@
 from generator import *
 import random
 choiceAdj = ["stronk","powerful","soprendente","triste","felicdad","Brave", "resilient", "determined","Passionate", "creative", "innovative","Kind", "compassionate", "empathetic","Adventurous", "spontaneous", "fearless", "Intelligent", "knowledgeable", "insightful", "Honest", "trustworthy", "reliable", "Generous", "selfless", "charitable", "Ambitious", "driven", "focused","Loyal", "supportive", "dependable","Talented", "skillful", "versatile"]
-choiceNouns = ["rat", "pigeon", "monkey", "swordfish", "dain", "Mountain", "river", "forest", "Car", "bike", "scooter", "Book", "magazine", "newspaper", "Dog", "cat", "bird", "Table", "chair", "lamp", "House", "apartment", "cottage", "Pen", "pencil", "marker", "Phone", "tablet", "laptop", "Pizza", "burger", "sandwich", "Tree", "flower", "bush"]
+choiceNou = ["rat", "pigeon", "monkey", "swordfish", "dain", "Mountain", "river", "forest", "Car", "bike", "scooter", "Book", "magazine", "newspaper", "Dog", "cat", "bird", "Table", "chair", "lamp", "House", "apartment", "cottage", "Pen", "pencil", "marker", "Phone", "tablet", "laptop", "Pizza", "burger", "sandwich", "Tree", "flower", "bush"]
 class Minh(Generator):
 
   
