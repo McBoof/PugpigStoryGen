e1df335160f778b8cf78dbeac2d0f64145f184f1
SuperDain1 <138471489+SuperDain1@users.noreply.github.com>
2023-07-03 13:59:03 +0000

    pee

--- dain.py
+++ dain.py
@@ -1,5 +1,15 @@
 from generator import *
+import random
+choiceadj = ["Delectable","Crispy","Dead","Yummy"]
+choicenou = ["Komodo Dragon","Human Flesh","Bomb"]
 class Dain(Generator):
 
+
+
   def getNoun(self):
-    return "Human flesh"
+    noun = random.choice(choicenou)
+    return noun
+
+  def getAdjective(self):
+    adjective = random.choice(choiceadj)
+    return adjective 
