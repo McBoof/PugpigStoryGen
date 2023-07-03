bf17df1df1ab5f232dfc0dc4d791013ee54783fd
NoahMGit <138467278+NoahMGit@users.noreply.github.com>
2023-07-03 14:00:09 +0000

    Changed Noah

--- noah.py
+++ noah.py
@@ -1,5 +1,26 @@
 from generator import *
+import random
 class Noah(Generator):
+  nouns = [
+  ["ladybird", "butterfly", "bird", "beetle", "squid"],
+  ["brick", "tree", "phone"],
+  ["London", "New York", "Melbourne", "Tokyo"]
+  ]
+
+  adjectives = [
+  ["cool", "slick", "awesome"],
+  ["slimy", "wet", "miniscule", "elephantine", "invisible", "bouncy"],
+  ["broken", "ugly"]
+  ]
   
   def getNoun(self):
-    return "cat"
+    type = Noah.nouns[random.randint(0, len(Noah.nouns)-1)]
+    noun = type[random.randint(0, len(type)-1)]
+    return noun
+
+  def getAdjective(self):
+    type = Noah.adjectives[random.randint(0, len(Noah.adjectives)-1)]
+    adjective = type[random.randint(0, len(type)-1)]
+    return adjective
+
+    
